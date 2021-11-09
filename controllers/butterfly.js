2274e7a782f1f9a1b24d9a67990875b8afffd540
Pragna Reddy Nathala <nathalapragnareddy@gmail.com>
2021-11-08 21:50:13 -0600

    added api's

--- controllers/butterfly.js
+++ controllers/butterfly.js
@@ -1,8 +1,12 @@
-var butterfly = require('../models/butterfly'); 
+const butterfly = require('../models/butterfly');
+
+
  
 // List of all butterflys 
-exports.butterfly_list = function(req, res) { 
-    res.send('NOT IMPLEMENTED: butterfly list'); 
+exports.butterfly_list = async function(req, res) { 
+
+   thebutterflys = await butterfly.find(); 
+    res.send(thebutterflys); 
 }; 
  
 // for a specific butterfly. 
@@ -11,8 +15,24 @@ exports.butterfly_detail = function(req, res) {
 }; 
  
 // Handle butterfly create on POST. 
-exports.butterfly_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: butterfly create POST'); 
+exports.butterfly_create_post =  async function(req, res) { 
+    console.log(req.body) 
+     let document = new butterfly(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"butterfly_type":"goat", "cost":12, "size":"large"} 
+    document.Size = req.body.Size; 
+    document.color = req.body.color; 
+    document.price = req.body.price; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }  
 }; 
  
 // Handle butterfly delete form on DELETE. 
@@ -23,4 +43,16 @@ exports.butterfly_delete = function(req, res) {
 // Handle butterfly update form on PUT. 
 exports.butterfly_update_put = function(req, res) { 
     res.send('NOT IMPLEMENTED: butterfly update PUT' + req.params.id); 
+}; 
+
+// List of all butterflys 
+exports.butterfly_view_all_Page = async function(req, res) { 
+    try{ 
+        thebutterflys = await butterfly.find(); 
+        res.render('butterfly', { title: 'butterfly Search Results', results: thebutterflys }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
--- routes/butterfly.js
+++ routes/butterfly.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const butterfly_controlers= require('../controllers/butterfly');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('butterfly', { title: 'Search Results Butterfly' });
-});
-
+/* GET butterflys */
+router.get('/', butterfly_controlers.butterfly_view_all_Page );
+router.post('/butterfly', butterfly_controlers.butterfly_create_post);
 module.exports = router;
