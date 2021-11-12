b242d5faf6c6fb2a9164cd17be986213a96a32a2
Pragna Reddy Nathala <nathalapragnareddy@gmail.com>
2021-11-11 19:50:51 -0600

    added routers

--- controllers/butterfly.js
+++ controllers/butterfly.js
@@ -10,8 +10,15 @@ exports.butterfly_list = async function(req, res) {
 }; 
  
 // for a specific butterfly. 
-exports.butterfly_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: butterfly detail: ' + req.params.id); 
+exports.butterfly_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await butterfly.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    }  
 }; 
  
 // Handle butterfly create on POST. 
@@ -36,13 +43,36 @@ exports.butterfly_create_post =  async function(req, res) {
 }; 
  
 // Handle butterfly delete form on DELETE. 
-exports.butterfly_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: butterfly delete DELETE ' + req.params.id); 
+exports.butterfly_delete =  async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await butterfly.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
 }; 
  
 // Handle butterfly update form on PUT. 
-exports.butterfly_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: butterfly update PUT' + req.params.id); 
+exports.butterfly_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+//  ${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await butterfly.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.Size)toUpdate.Size = req.body.Size; 
+        if(req.body.color)toUpdate.color = req.body.color; 
+        if(req.body.price)toUpdate.price = req.body.price; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
 }; 
 
 // List of all butterflys 
